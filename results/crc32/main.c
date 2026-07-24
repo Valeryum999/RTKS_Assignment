b5da9bd0a38aca10e3ecf2598110883645280e34
uncavo-hdmi <davide.saladino@studenti.unipd.it>
2026-07-24 14:34:01 +0200

    fix: change crc32 impl to match the optimized one created by clang

--- results/crc32/main.c
+++ results/crc32/main.c
@@ -8,6 +8,7 @@
 #define BUFFER_SIZE (32 * 1024)
 
 static uint8_t buffer[BUFFER_SIZE];
+static uint32_t crc_table[256];
 
 volatile uint32_t benchmark_result = 0;
 
@@ -19,21 +20,22 @@ void bench_init(void)
         x = x * 1664525u + 1013904223u;
         buffer[i] = (uint8_t)(x >> 24);
     }
+
+    for (uint32_t i = 0; i < 256; i++) {
+        uint32_t c = i;
+        for (int k = 0; k < 8; k++)
+            c = (c & 1) ? (0xEDB88320u ^ (c >> 1)) : (c >> 1);
+        crc_table[i] = c;
+    }
 }
 
 static uint32_t crc32(const uint8_t *data, uint32_t length)
 {
     uint32_t crc = 0xFFFFFFFF;
 
-    for (uint32_t i = 0; i < length; i++) {
-        crc ^= data[i];
-        for (int bit = 0; bit < 8; bit++) {
-            if (crc & 1)
-                crc = (crc >> 1) ^ 0xEDB88320;
-            else
-                crc >>= 1;
-        }
-    }
+    for (uint32_t i = 0; i < length; i++)
+        crc = crc_table[(crc ^ data[i]) & 0xFF] ^ (crc >> 8);
+
     return ~crc;
 }
 
